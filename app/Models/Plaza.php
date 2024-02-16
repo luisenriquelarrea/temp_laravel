f94251ca223f13f3bc359a3e79a402108398fb0d
Luis Enrique Larrea <enrique.larrea@alumno.udg.mx>
2024-02-15 21:25:50 -0600

    RFX

--- app/Models/Plaza.php
+++ app/Models/Plaza.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Plaza extends Model
 {
     use HasFactory;
     protected $table = 'plaza';
+
+    /**
+     * Get the empleados for the plaza.
+     */
+    public function empleados(): HasMany
+    {
+        return $this->hasMany(Empleado::class);
+    }
 }
