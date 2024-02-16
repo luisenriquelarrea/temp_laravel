98fe4e90215d6d3c33f67b63b62d3115db8e8619
Luis Enrique Larrea <enrique.larrea@alumno.udg.mx>
2024-02-15 22:23:03 -0600

    RFX

--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Empleado extends Model
 {
     use HasFactory;
     protected $table = 'empleado';
+
+    /**
+     * Get the post that owns the comment.
+     */
+    public function plaza(): BelongsTo
+    {
+        return $this->belongsTo(Plaza::class);
+    }
 }
