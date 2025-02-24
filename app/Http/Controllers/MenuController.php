3b8f177d1c2b096b3df5769385341275bbfbc8d7
luisenriquelarreamx <luis.larrea@montes.com.mx>
2025-02-24 17:32:00 -0600

    RFX

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,19 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
+use App\Http\Filters\MenuFilter;
+use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    /**
-     * Constructor.
-     *
-     */
-    public function __construct(){
-        $modelo = new Menu;
-        $tabla = 'menu';
-        parent::__construct($modelo, $tabla);
-    }
-
     /**
      * Display a listing of the resource.
      *
@@ -23,6 +15,73 @@ class MenuController extends Controller
      */
     public function index()
     {
-        return Menu::select('menu.*')->orderBy('id', 'ASC')->get();
+        $records = Menu::all();
+        return response()->json($records, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $record = Menu::create($request->all());
+        return response()->json($record, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(int $id)
+    {
+        $record = Menu::find($id);
+        if (!$record)
+            return response()->json(['message' => 'record not found'], 404);
+        return response()->json($record, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, int $id)
+    {
+        $record = Menu::findOrFail($id);
+        $record->update($request->all());
+        return response()->json($record, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $id)
+    {
+        $record = Menu::findOrFail($id);
+        $record->delete();
+        return response()->json(null, 204);
+    }
+
+    /**
+     * Display a listing of the resource filtered.
+     * 
+     * @param  MenuFilter $filters
+     * @return \Illuminate\Http\Response
+     */
+    public function filteredList(MenuFilter $filters)
+    {
+        $records = Menu::query();
+        $filteredRecords = $filters->apply($records);
+        return response()->json($filteredRecords, 200);
     }
 }
--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,11 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    use HasFactory;
     protected $table = 'menu';
+
+    protected $guarded = [
+        'id', 
+        'created_at', 
+        'updated_at'
+    ];
+
+    protected $fillable = [
+        'descripcion',
+        'label',
+        'icon',
+        'orden',
+        'user_created_id', 
+        'user_updated_id'
+    ];
 }
